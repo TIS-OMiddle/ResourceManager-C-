4a73b1e6138bc8e5e0fa9773368c2bb34c4280e2
codeflame <1124071636@qq.com>
2018-05-28 13:18:30 +0800

    treeview与地址栏同步变化

--- WpfTest/MainWindow.xaml.cs
+++ WpfTest/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MyControlItem;
 
 namespace WpfTest {
     /// <summary>
@@ -21,6 +22,7 @@ namespace WpfTest {
     public partial class MainWindow : Window {
 
         private string CurrentPath = "";
+        private bool TVChangeByOther = false;
 
         public MainWindow() {
             InitializeComponent();
@@ -39,77 +41,30 @@ namespace WpfTest {
             string[] disks = Directory.GetLogicalDrives();
             DriveInfo[] disksInfo = DriveInfo.GetDrives();
             int j = 0;
-            MyTreeViewItem root = new MyTreeViewItem("此电脑", "folder");
+            MyTreeViewItem root = new MyTreeViewItem("此电脑", MyIcons.disk);
             root.Tag = "";
             foreach (string i in disks) {
                 MyTreeViewItem tvic = new MyTreeViewItem(
-                    disksInfo[j++].VolumeLabel + " (" + i.Substring(0, 2) + ")", "folder");
+                    disksInfo[j++].VolumeLabel + " (" + i.Substring(0, 2) + ")", MyIcons.disk);
                 tvic.Tag = i;
                 root.Items.Add(tvic);
             }
             return root;
         }
 
-        //为树结点创建子结点
-        public void CreatSubTVItemFromTVItem(MyTreeViewItem item) {
-            try {
-                string newpath = item.Tag.ToString();
-                string[] dirs = Directory.GetDirectories(newpath);//获取该目录的内容
-                if (newpath.Length > 3) newpath += "\\"; ;
-                foreach (string i in dirs) {
-                    if ((new FileInfo(i).Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
-                    MyTreeViewItem tvic = new MyTreeViewItem(i.Replace(newpath, ""), "folder");
-                    tvic.Tag = i;
-                    item.Items.Add(tvic);
-                }
-            }
-            catch (Exception) { }
-        }
-
-        //根据路径返回listbox的Item集合
-        public List<MyListBoxItem> CreatLBItemFromPath(string path) {
-            List<MyListBoxItem> res = new List<MyListBoxItem>();
-            if (path == "") {
-                DriveInfo[] disksInfo = DriveInfo.GetDrives();
-                int j = 0;
-                foreach (var i in disksInfo) {
-                    MyListBoxItem lbit = new MyListBoxItem(
-                        disksInfo[j].VolumeLabel + " (" + i.Name.Substring(0, 2) + ")", "folder");
-                    lbit.MyMessage = "可用空间:" + (disksInfo[j].TotalFreeSpace/1024/1024/1024).ToString()+"GB"
-                        + "\n总大小:"+(disksInfo[j].TotalSize / 1024 / 1024 / 1024).ToString() + "GB";
-                    res.Add(lbit);
-                    j++;
-                }
-                return res;
-            }
-            DirectoryInfo _files = new DirectoryInfo(path);
-            FileInfo[] files = _files.GetFiles("*", SearchOption.TopDirectoryOnly);
-            DirectoryInfo[] dirs = _files.GetDirectories("*", SearchOption.TopDirectoryOnly);
-            foreach (var i in files) {
-                if ((i.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
-                MyListBoxItem lbit = new MyListBoxItem(i.Name, "file");
-                lbit.MyMessage = i.Name + "\n类型:" + i.Extension + "文件\n修改日期:" + i.LastWriteTime.ToString();
-                res.Add(lbit);
-            }
-            foreach (var i in dirs) {
-                if ((i.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
-                MyListBoxItem lbit = new MyListBoxItem(i.Name, "folder");
-                lbit.MyMessage = i.Name + "\n创建日期:" + i.CreationTime;
-                res.Add(lbit);
-            }
-            return res;
-        }
 
         //treeview选中项改变事件
         private void tv_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            if (TVChangeByOther) return;//不来自tv本身的选择改变则直接退出
+
             MyTreeViewItem my_select = (MyTreeViewItem)tv.SelectedItem;
             CurrentPath = my_select.Tag.ToString();
-            if (my_select.Items.Count == 0) {
+            if (my_select.Items.IsEmpty) {
                 tv.BeginInit();
-                CreatSubTVItemFromTVItem(my_select);
+                MyItemManager.CreatSubTVItemFromTVItem(my_select);
                 tv.EndInit();
             }
-            List<MyListBoxItem> list = CreatLBItemFromPath(CurrentPath);
+            List<MyListBoxItem> list = MyItemManager.CreatLBItemFromPath(CurrentPath);
             lb.BeginInit();
             lb.Items.Clear();
             foreach (var i in list)
@@ -119,7 +74,7 @@ namespace WpfTest {
             else address_box.Text = CurrentPath;
         }
 
-        //listbox双击进入或打开
+        //listbox双击进入或打开事件
         private void lb_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
             if (e.OriginalSource.GetType() != typeof(ScrollViewer)&&e.RightButton!=MouseButtonState.Pressed) {
                 var m = (MyListBoxItem)lb.SelectedItem;
@@ -137,7 +92,7 @@ namespace WpfTest {
                     }
                     else CurrentPath += filename;//双击项为目录
                 }
-                else {//当前路径不为盘符不加斜杠
+                else {//当前路径不为盘符加斜杠
                     CurrentPath = CurrentPath + "\\";
                     if (File.Exists(CurrentPath + filename)) {//双击项为文件
                         System.Diagnostics.Process.Start(CurrentPath + filename);
@@ -145,14 +100,18 @@ namespace WpfTest {
                     }
                     else CurrentPath += filename;//双击项为目录
                 }
-
+                //刷新listbox
                 FlushLBByCurrentPath();
-                if (CurrentPath == "") address_box.Text = "此电脑";
-                else address_box.Text = CurrentPath;
+                //刷新地址栏
+                address_box.Text = CurrentPath;
+                //使treeview选中项同步变化
+                TVChangeByOther = true;
+                MyItemManager.GetTVItemFromPath(tv, CurrentPath, true);
+                TVChangeByOther = false;
             }
         }
 
-        //listbox空白区域点击取消选中
+        //listbox空白区域点击取消选中事件
         private void lb_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
             if (e.OriginalSource.GetType() == typeof(ScrollViewer)) {
                 var item = lb.SelectedItem as MyListBoxItem;
@@ -166,14 +125,14 @@ namespace WpfTest {
         //由CurrentPath变量刷新listbox
         private void FlushLBByCurrentPath() {
             lb.BeginInit();
-            var list = CreatLBItemFromPath(CurrentPath);
+            var list = MyItemManager.CreatLBItemFromPath(CurrentPath);
             lb.Items.Clear();
             foreach (var i in list)
                 lb.Items.Add(i);
             lb.EndInit();
         }
 
-        //lisibox键盘，监控快捷键F2，重命名用
+        //lisibox键盘，监控快捷键F2，重命名用事件
         private void lb_KeyDown(object sender, KeyEventArgs e) {
             if (e.Key == Key.F2) {
                 var item = lb.SelectedItem as MyListBoxItem;
@@ -184,7 +143,7 @@ namespace WpfTest {
             }
         }
 
-        //listbox子控件textbox，重命名用
+        //listbox子控件textbox，重命名用事件
         private void TextBox_KeyDown(object sender, KeyEventArgs e) {
             try {
                 if (e.Key == Key.Enter) {
@@ -203,139 +162,5 @@ namespace WpfTest {
             catch (Exception) { }
         }
 
-    }
-
-    public class MyListBoxItem {
-        private static byte[] folder;
-        private static byte[] file;
-        static MyListBoxItem() {
-            FileStream fs = new FileStream("./icon/folder.ico", FileMode.Open);
-            folder = new byte[fs.Length];
-            fs.Read(folder, 0, folder.Length);
-            fs.Close();
-
-            //folder图标初始化
-            fs = new FileStream("./icon/file.ico", FileMode.Open);
-            file = new byte[fs.Length];
-            fs.Read(file, 0, file.Length);
-            fs.Close();
-        }
-
-        public BitmapImage MyIcon { set; get; }
-        public string MyText { set; get; }
-        public string MyMessage { set; get; }
-        public Visibility MyVisialbe { set; get; }
-
-        public MyListBoxItem(string text,string icon) {
-            MyText = text;
-            MyIcon = new BitmapImage();
-            MyIcon.BeginInit();
-            MyVisialbe = Visibility.Hidden;
-            switch (icon) {
-                case "folder":
-                    MyIcon.StreamSource = new MemoryStream(folder);
-                    break;
-                case "file":
-                    MyIcon.StreamSource = new MemoryStream(file);
-                    break;
-                default:
-                    MyIcon.StreamSource = new MemoryStream(folder);
-                    break;
-            }
-            MyIcon.EndInit();
-        }
-    }
-
-    //带有图标-文本的重载控件
-    public class MyTreeViewItem : TreeViewItem {
-        private static byte[] folder;
-        private static byte[] file;
-
-        static MyTreeViewItem() {
-            //folder图标初始化
-            FileStream fs = new FileStream("./icon/folder.ico", FileMode.Open);
-            folder = new byte[fs.Length];
-            fs.Read(folder, 0, folder.Length);
-            fs.Close();
-
-            //folder图标初始化
-            fs = new FileStream("./icon/file.ico", FileMode.Open);
-            file = new byte[fs.Length];
-            fs.Read(file, 0, file.Length);
-            fs.Close();
-        }
-
-        private TextBlock tb;
-        private Image img;
-        private StackPanel sp;
-
-        public MyTreeViewItem() {
-            tb = new TextBlock();
-            sp = new StackPanel();
-            sp.Orientation = System.Windows.Controls.Orientation.Horizontal;
-            Header = sp;
-        }
-
-        //text:显示的文本  icon:选择显示的图标
-        public MyTreeViewItem(string text, string icon) {
-            tb = new TextBlock();
-            sp = new StackPanel();
-            sp.Orientation = System.Windows.Controls.Orientation.Horizontal;
-            Header = sp;
-            tb.Text = text;
-            HeaderIcon = icon;
-        }
-
-        public string HeaderText {
-            get { return tb.Text; }
-            set { tb.Text = value; }
-        }
-
-        public string HeaderIcon {
-            set {
-                BitmapImage icon = new BitmapImage();
-                icon.BeginInit();
-                icon.DecodePixelWidth = 13;
-                icon.DecodePixelHeight = 13;
-                switch (value) {
-                    case "folder":
-                        icon.StreamSource = new MemoryStream(folder);
-                        break;
-                    case "file":
-                        icon.StreamSource = new MemoryStream(file);
-                        break;
-                    default:
-                        icon.StreamSource = new MemoryStream(file);
-                        break;
-                }
-                icon.EndInit();
-                img = new Image();
-                img.Source = icon;
-                img.Margin = new Thickness(0, 0, 5, 0);
-                sp.Children.Clear();
-                sp.Children.Add(img);
-                sp.Children.Add(tb);
-            }
-        }
-    }
-
-    //文本缩短显示
-    public class StringformatConvert : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-            string s = value.ToString();
-            int leng;
-            if (int.TryParse(parameter.ToString(), out leng)) {
-                if (s.Length <= leng)
-                    return s;
-                else
-                    return s.Substring(0, leng) + "...";
-            }
-            else
-                return string.Empty;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-            throw new NotImplementedException();
-        }
     }
 }
